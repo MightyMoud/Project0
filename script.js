78910ee89cbc2e9367c21549511481d54f851f5d
Mahmoud shehata <CReative59@Mahmouds-MacBook-Pro.local>
2019-10-02 20:12:21 +1000

    Initial Release

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        autoplay: true,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        slidesToShow: 1
+    });
+});
