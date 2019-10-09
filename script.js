ad1b4b9724835e28169449e1b60919fb6080cb68
Ms-Mousa <Contact@mahmoud.blog>
2019-10-10 10:54:09 +1100

    added nav for mobile devices. Improved responsiveness

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ $(document).ready(function() {
     $('.slider').slick({
         dots: true,
         sliderToShow: 2,
+        autoplay: true,
         adaptiveHeight: true
     });
 })
+
+menuClick();
+
+function menuClick() {
+    let x = document.querySelector(".menu");
+    x.addEventListener("click", showMenu => {
+        const y = document.querySelectorAll(".menu--hidden");
+        $(y).slideToggle();
+    });
+}
+
+$(window).scroll(function() {
+    var distanceFromTop = $(this).scrollTop();
+    if (distanceFromTop <= $('.topLinks').height()) {
+        $('.topLinks').css({
+            overflow: "hidden",
+            position: "relative",
+        });
+    } else {
+        $('.topLinks').css({
+            overflow: "initial",
+            position: "fixed",
+        });
+    }
+});
